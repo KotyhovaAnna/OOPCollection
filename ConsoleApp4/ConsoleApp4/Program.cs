a415bd2db2383bc743b0fbc7761eb7a548f77d91
KotyhovaAnna <anna.kotihova@mail.ru>
2020-05-13 23:53:36 +0300

    Add collection

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace OOPжанры
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Фантастика> k = new List<Фантастика>();
+            Фантастика Гарри_Поттер = new Фантастика();
+            Гарри_Поттер.namef = "Гарри Поттер. Орден Феникса";
+            Гарри_Поттер.namedirector = "Дэвид Йейтс";
+            Фантастика Властелин_Колец = new Фантастика();
+            Властелин_Колец.namef = "Властелин Колец.Братство Кольца";
+            Властелин_Колец.namedirector = "Питер Джексон";
+            Фантастика Начало = new Фантастика();
+            Начало.namedirector = "Кристофер Нолан";
+            k.Add(Гарри_Поттер);
+            k.Add(Властелин_Колец);
+            k.Add(Начало);
+            var orderedФантастика = from i in k
+                                    orderby i.namedirector
+                                    select i;
+
+            foreach (Фантастика i in orderedФантастика)
+            {
+                Console.WriteLine(i.namedirector);
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Фильмы.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace ConsoleAppOOP
+{
+    abstract class Фильмы
+    {
+        string название;
+        int Год_выпуска;
+        public abstract void Продолжительность();
+        public void getназвание()
+        {
+            Console.WriteLine(название);
+        }
+    }
+}
